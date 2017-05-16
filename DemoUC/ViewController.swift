7fff038e327bba0bb632742b346f4ee163bb51e3
Роман Тузин <tuzinroman@mac-mini-roman.local>
2017-05-16 20:31:14 +0700

    Initial Commit

---
+++ DemoUC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoUC
+//
+//  Created by Роман Тузин on 16.05.17.
+//  Copyright © 2017 The Best. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
