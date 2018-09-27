13d0af4666d4eb454f30cf797e9689e6014f1e4c
RomanXXI <tra-ss@mail.ru>
2018-09-27 17:39:54 +0700

    - Текущее состояние

--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@
 
 target 'Sminex' do
     
-    platform :ios, '9.1'
+    platform :ios, '10.0'
     
     # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
     use_frameworks!
@@ -26,9 +26,9 @@ target 'Sminex' do
 end
 
 # Workaround for Cocoapods issue #7606
-post_install do |installer|
-    installer.pods_project.build_configurations.each do |config|
-        config.build_settings.delete('CODE_SIGNING_ALLOWED')
-        config.build_settings.delete('CODE_SIGNING_REQUIRED')
-    end
-end
+#post_install do |installer|
+#    installer.pods_project.build_configurations.each do |config|
+#        config.build_settings.delete('CODE_SIGNING_ALLOWED')
+#        config.build_settings.delete('CODE_SIGNING_REQUIRED')
+#    end
+#end
--- Sminex/AppDelegate.swift
+++ Sminex/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 import UserNotifications
-//import Firebase
+import Firebase
 import FirebaseInstanceID
 import FirebaseMessaging
 import YandexMobileMetrica
@@ -21,40 +21,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-//        if #available(iOS 10.0, *) {
-//            // For iOS 10 display notification (sent via APNS)
-//            UNUserNotificationCenter.current().delegate = self as! UNUserNotificationCenterDelegate
-//            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//            UNUserNotificationCenter.current().requestAuthorization(
-//                options: authOptions,
-//                completionHandler: {_, _ in })
-//            // For iOS 10 data message (sent via FCM
-//            Messaging.messaging().remoteMessageDelegate = self as! MessagingDelegate
-//        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-//        }
-        
+        FirebaseApp.configure()
         application.registerForRemoteNotifications()
-        // TODO: - Uncomment this before release
-        //FirebaseApp.configure()
+        requestNotificationAuthorization(application: application)
         
         // Инициализация AppMetrica SDK
-//        YMMYandexMetrica.activate(with: YMMYandexMetricaConfiguration.init(apiKey: "5e18cb69-5852-4a9e-9229-c2a2b7c1bf52")!)
-//        if self === AppDelegate.self {
-            // Создание объекта конфигурации
-            let configuration = YMMYandexMetricaConfiguration.init(apiKey: "5e18cb69-5852-4a9e-9229-c2a2b7c1bf52")
-            // Реализуйте логику определения того, является ли запуск приложения первым. В качестве критерия вы можете использовать проверку наличия каких-то файлов (настроек, баз данных и др.), которые приложение создает в свой первый запуск
-            let isFirstApplicationLaunch = false
-            // Отслеживание новых пользователей
-            configuration?.handleFirstActivationAsUpdate = isFirstApplicationLaunch == false
-            // Отслеживание аварийной остановки приложений
-            configuration?.crashReporting = true
-            // Инициализация AppMetrica SDK
-            YMMYandexMetrica.activate(with: configuration!)
-//        }
+        // Создание объекта конфигурации
+        let configuration = YMMYandexMetricaConfiguration.init(apiKey: "5e18cb69-5852-4a9e-9229-c2a2b7c1bf52")
+        // Реализуйте логику определения того, является ли запуск приложения первым. В качестве критерия вы можете использовать проверку наличия каких-то файлов (настроек, баз данных и др.), которые приложение создает в свой первый запуск
+        let isFirstApplicationLaunch = false
+        // Отслеживание новых пользователей
+        configuration?.handleFirstActivationAsUpdate = isFirstApplicationLaunch == false
+        // Отслеживание аварийной остановки приложений
+        configuration?.crashReporting = true
         
+
         return true
     }
     
@@ -87,5 +68,76 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
+    
+    var applicationStateString: String {
+        if UIApplication.shared.applicationState == .active {
+            return "active"
+        } else if UIApplication.shared.applicationState == .background {
+            return "background"
+        }else {
+            return "inactive"
+        }
+    }
+    
+    func requestNotificationAuthorization(application: UIApplication) {
+        Messaging.messaging().delegate = self
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print(token)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NSLog("didFailToRegisterForRemoteNotificationsWithError")
+    }
+    
+}
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    // iOS10+, called when presenting notification in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        NSLog("[UserNotificationCenter] applicationState: \(applicationStateString) willPresentNotification: \(userInfo)")
+        //TODO: Handle foreground notification
+        completionHandler([.alert])
+    }
+    
+    // iOS10+, called when received response (default open, dismiss or custom action) for a notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        NSLog("[UserNotificationCenter] applicationState: \(applicationStateString) didReceiveResponse: \(userInfo)")
+        //TODO: Handle background notification
+        completionHandler()
+    }
+}
+
+extension AppDelegate : MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        NSLog("[RemoteNotification] didRefreshRegistrationToken: \(fcmToken)")
+    }
+    
+    // iOS9, called when presenting notification in foreground
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        NSLog("[RemoteNotification] applicationState: \(applicationStateString) didReceiveRemoteNotification for iOS9: \(userInfo)")
+        if UIApplication.shared.applicationState == .active {
+            //TODO: Handle foreground notification
+        } else {
+            //TODO: Handle background notification
+        }
+    }
 }
 
