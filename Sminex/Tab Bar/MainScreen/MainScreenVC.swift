cc3bcd2df47ef2fa6d09d45fbd186c56776f0b11
RomanXXI <tra-ss@mail.ru>
2019-01-09 20:55:09 +0700

    - новый релиз

--- Sminex/Tab Bar/MainScreen/MainScreenVC.swift
+++ Sminex/Tab Bar/MainScreen/MainScreenVC.swift
@@ -1465,7 +1465,7 @@ final class CellsHeader: UICollectionReusableView {
             self.detail.setTitle("Подробнее", for: .normal)
         } else if item.title == "Версия" {
             self.detail.setTitleColor(UIColor.black, for: .normal)
-            self.detail.setTitle("ver. 1.83", for: .normal)
+            self.detail.setTitle("ver. 1.85", for: .normal)
         } else {
             self.detail.setTitle("Все", for: .normal)
         }
