6c9bc9dae50d0ef062747793fe7447428cd192f9
Sminex <sminexapp@sminex.com>
2019-05-21 18:44:46 +0300

    1.94

--- Sminex/Tab Bar/MainScreen/MainScreenVC.swift
+++ Sminex/Tab Bar/MainScreen/MainScreenVC.swift
@@ -1520,7 +1520,7 @@ final class CellsHeader: UICollectionReusableView {
             self.detail.setTitleColor(self.tintColor, for: .normal)
         } else if item.title == "Версия" {
             self.detail.setTitleColor(UIColor.black, for: .normal)
-            self.detail.setTitle("ver. 1.93", for: .normal)
+            self.detail.setTitle("ver. 1.94", for: .normal)
         } else {
             self.detail.setTitle("Все", for: .normal)
             self.detail.setTitleColor(self.tintColor, for: .normal)
