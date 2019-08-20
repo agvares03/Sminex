58b59c9d9aa2107b0ef1038bb76a10a86b9fd8cd
Sergey Ivanov <plastilin1998@gmail.com>
2019-08-20 16:49:35 +0800

    -

--- Sminex/Tab Bar/Contacts/ContactsVC.swift
+++ Sminex/Tab Bar/Contacts/ContactsVC.swift
@@ -103,7 +103,7 @@ final class ContactsVC: UIViewController, UICollectionViewDelegate, UICollection
                 return CGSize(width: view.frame.size.width, height: 0)
             }
         }
-//        if (data_[indexPath.row].name?.contains(find: "Предложения"))! {
+        if (data_[indexPath.row].name?.contains(find: "Предложения"))! {
 //            if Device() == .iPhoneSE || Device() == .simulator(.iPhoneSE) || Device() == .iPhone5s || Device() == .simulator(.iPhone5s) || Device() == .iPhone5 || Device() == .simulator(.iPhone5) || Device() == .iPhone4s || Device() == .simulator(.iPhone4s) || Device() == .iPhone5 || Device() == .simulator(.iPhone5) || Device() == .iPhone5c || Device() == .simulator(.iPhone5c){
 //                return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 30)
 //            }else if Device() == .iPhoneX || Device() == .simulator(.iPhoneX) {
@@ -111,7 +111,18 @@ final class ContactsVC: UIViewController, UICollectionViewDelegate, UICollection
 //            }else{
 //                return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 40)
 //            }
-//        }
+            return CGSize(width: view.frame.size.width, height: isSupport ? size.height : size.height - 10)
+        }
+        if (data_[indexPath.row].name?.contains(find: "Консьерж"))! {
+            //            if Device() == .iPhoneSE || Device() == .simulator(.iPhoneSE) || Device() == .iPhone5s || Device() == .simulator(.iPhone5s) || Device() == .iPhone5 || Device() == .simulator(.iPhone5) || Device() == .iPhone4s || Device() == .simulator(.iPhone4s) || Device() == .iPhone5 || Device() == .simulator(.iPhone5) || Device() == .iPhone5c || Device() == .simulator(.iPhone5c){
+            //                return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 30)
+            //            }else if Device() == .iPhoneX || Device() == .simulator(.iPhoneX) {
+            //                return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 40)
+            //            }else{
+            //                return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 40)
+            //            }
+            return CGSize(width: view.frame.size.width, height: isSupport ? size.height : size.height)
+        }
 //        return CGSize(width: view.frame.size.width, height: isSupport ? size.height - 10 : size.height - 20)
         return CGSize(width: view.frame.size.width, height: isSupport ? size.height : size.height - 20)
     }
